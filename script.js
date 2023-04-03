03025dab3a482ef38548c4adf96b4a726547f736
nde95 <ndennison268@gmail.com>
2023-04-03 10:34:46 -0400

    final update
    added window load functionality, randomize button and updated scripts

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-let setGradient = function() {
-	body.style.background = "linear-gradient(to right, " 
+// random button
+
+
+let randomize = document.getElementsByClassName("random")[0];
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+function RBC (e) {
+  color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+  color2.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+  	body.style.background = "linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
@@ -12,6 +20,20 @@ let setGradient = function() {
 
 	css.textContent = body.style.background + ";";
 }
+randomize.addEventListener("click", RBC);
+window.onload = RBC();
+
+
+// background changing
+// let setGradient = function() {
+// 	body.style.background = "linear-gradient(to right, " 
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+
+// 	css.textContent = body.style.background + ";";
+// }
 
 // left
 color1.addEventListener("input", setGradient);
