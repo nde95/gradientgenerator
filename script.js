581e9f881b532e8f9072fde0e3e5348ee866a198
nde95 <ndennison268@gmail.com>
2023-04-04 13:05:42 -0400

    final code update
    modernized and cleaned up code js file

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const randomizeButton = document.querySelector(".random");
 
-// random button
-
-
-let randomize = document.getElementsByClassName("random")[0];
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-function RBC (e) {
-  color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
-  color2.value = '#'+Math.floor(Math.random()*16777215).toString(16);
-  	body.style.background = "linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = body.style.background + ";";
-}
-randomize.addEventListener("click", RBC);
-window.onload = RBC();
-
-
-// background changing
-let setGradient = function() {
-	body.style.background = "linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = body.style.background + ";";
+function generateRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  css.textContent = body.style.background + ";";
 }
 
-// left
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = body.style.background + ";";
+}
+
+function randomizeColors() {
+  color1.value = generateRandomColor();
+  color2.value = generateRandomColor();
+  setGradient();
+}
+
+randomizeButton.addEventListener("click", randomizeColors);
 color1.addEventListener("input", setGradient);
-
-// right
 color2.addEventListener("input", setGradient);
+
+window.onload = randomizeColors();
